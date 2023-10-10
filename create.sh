d37d74df6c2d3083ad8c83c1537880625af40325
SatyrDiamond <109001272+SatyrDiamond@users.noreply.github.com>
2023-10-10 18:47:32 -0400

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
-ffmpeg -i 'Crunk(Demo).wav' -i lmoous.png -y -filter_complex " \
+ffmpeg -i '/home/colby/Music/introzik.xm' -i lmoous.png -y -filter_complex " \
 [0:a] volume=0.2,showwaves=s=500x500:mode=line:s=500x500:colors=#ee55e8@1:scale=sqrt:r=30,format=rgba [waveform]; \
 [1:v] format=rgba [textimage]; \
-[waveform][textimage] overlay=W-w:H-h:enable='between(t,0,20)'" -b:v 8000k final-inv.mp4
+[waveform][textimage] overlay=W-w:H-h" -b:v 8000k final-inv.mp4
 
 ffmpeg -y -i final-inv.mp4 -vf negate -b:v 8000k final.mp4
